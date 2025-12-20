507c5fba899e342a36d01ad6bd3edff13729bbfd
k_funaki <k_funaki@nexus-nt.co.jp>
2025-12-20 23:08:40 +0900

    Force dynamic rendering for auth callback route

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -2,6 +2,8 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
