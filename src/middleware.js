99de8bc13ae790b6107fde08a03477c1151aedfe
k_funaki <k_funaki@nexus-nt.co.jp>
2025-12-28 02:00:58 +0900

    Exclude callback route from middleware to prevent cookie interference

--- src/middleware.js
+++ src/middleware.js
@@ -14,6 +14,6 @@ export const config = {
          * - api routes
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|api|callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
